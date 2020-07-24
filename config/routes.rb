c4478eea8811c8d35dd65388e913d7a855f1da71
isivaldan <isi.valenzuela.d@gmail.com>
2020-07-24 14:05:57 -0400

    tercer commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root'pages#one'
-
+  
+  get '/one', to:'pages#one'
   get '/two', to:'pages#two'
   get '/three', to:'pages#three'
+  root'pages#one'
 end
