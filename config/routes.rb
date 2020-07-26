bbf9eea5ca29e106f32409151764e731e89948dc
isivaldan <isi.valenzuela.d@gmail.com>
2020-07-26 19:25:25 -0400

    commit 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  get '/one', to:'pages#one'
-  get '/two', to:'pages#two'
-  get '/three', to:'pages#three'
+
+  get 'pages/two', to:'pages#two'
+  get 'pages/three', to:'pages#three'
   root'pages#one'
 end
